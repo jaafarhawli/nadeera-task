60507aa9cbc1e8daa5ce8a062286f77fa89cf810
JaafarHawli <jaafarhawle@gmail.com>
2022-12-01 20:24:11 +0200

    backend: added users migration

--- backend/database/migrations/2022_12_01_165618_create_users_table.php
+++ backend/database/migrations/2022_12_01_165618_create_users_table.php
@@ -1,1 +1,25 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->unsignedInteger('id')->unique();
+            $table->string('name');
+            $table->datetime('date_of_birth');
+            $table->string('profile_picture');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
