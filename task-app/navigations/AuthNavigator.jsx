efd23f6e30111112253eb40fa2c0a550fd96c94c
JaafarHawli <jaafarhawle@gmail.com>
2022-12-02 01:58:23 +0200

    app: added register page to auth navigator

--- task-app/navigations/AuthNavigator.jsx
+++ task-app/navigations/AuthNavigator.jsx
@@ -1,5 +1,5 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import Login  from '../screens/Auth/Login/Login';
+import { Login, Register } from '../screens';
 
 const Stack = createStackNavigator();
 
@@ -7,6 +7,7 @@ function AuthNavigator() {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName={'Login'}>
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
     </Stack.Navigator>
   );
 }
