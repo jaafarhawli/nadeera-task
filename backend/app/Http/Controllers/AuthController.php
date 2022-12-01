bc32a8b1d3fa0289da99fc8240350746eb9f89d6
JaafarHawli <jaafarhawle@gmail.com>
2022-12-01 20:23:35 +0200

    backend: configured jwt

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use JWTAuth;
+
+class AuthController extends Controller
+{
+
+
+}
