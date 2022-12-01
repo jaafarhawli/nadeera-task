2dc56b380e93915ee3ff68a3863c37f7ed2227a7
JaafarHawli <jaafarhawle@gmail.com>
2022-12-02 01:53:52 +0200

    app: added stack navigator

---
+++ task-app/navigations/AuthNavigator.jsx
@@ -0,0 +1,14 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import Login  from '../screens/Auth/Login/Login';
+
+const Stack = createStackNavigator();
+
+function AuthNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName={'Login'}>
+      <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
+  );
+}
+
+export default AuthNavigator;
