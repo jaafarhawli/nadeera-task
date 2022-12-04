e0622b9bd7b7f23db97adf9bc014f71d1b2dc108
JaafarHawli <jaafarhawle@gmail.com>
2022-12-04 17:17:59 +0200

    backend: removed user controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,13 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TodoListController;
-use App\Http\Controllers\UserController;
 
 Route::group(["prefix"=> "v1"], function() {
     
     Route::post('login', [AuthController::class, "login"]);
     Route::post('register', [AuthController::class, "register"]);
-    Route::post('/profile', [UserController::class, "uploadImage"]);
 
     Route::group(["middleware" => "auth:api"], function() {
         Route::post('/valid', [AuthController::class, "login"]);
